d5d4fe1843ae2fbf196e035ca49a7d36928edfc3
Dimitar Radenkov <dimitar.radenkov1@gmail.com>
2019-02-14 11:22:33 +0200

    added swipe gestures up, down for calendar view

---
+++ ExpenseTracker.Mobile/ExpenseTracker.Mobile/Controls/CalendarView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExpenseTracker.Mobile.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CalendarView : ContentView
+	{
+		public CalendarView()
+		{
+			InitializeComponent();
+        }
+
+        private void OnSwipeUp(object sender, SwipedEventArgs e)
+        {
+            this.BackgroundColor = Color.Blue;   
+        }
+
+        private void OnSwipeDown(object sender, SwipedEventArgs e)
+        {
+            this.BackgroundColor = Color.Gray;
+        }
+    }
+}
